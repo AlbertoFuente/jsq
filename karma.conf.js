28614144438757538b68a090742d7b5ce1a8b3e6
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-09-02 21:13:10 +0200

    Improve karma config

--- karma.conf.js
+++ karma.conf.js
@@ -10,11 +10,12 @@ module.exports = function(config) {
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine'],
+        frameworks: ['jasmine', 'sinon'],
 
 
         // list of files / patterns to load in the browser
         files: [
+            'app/*',
             'specs/*'
         ],
 
@@ -53,7 +54,7 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome', 'Firefox'],
+        browsers: ['Chrome'],
 
 
         // Continuous Integration mode
