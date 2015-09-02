5a7fa1f7324b0bef0a90d467eaf542d146bfae1b
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-09-02 21:13:38 +0200

    Working on TDD

---
+++ specs/tests.js
@@ -0,0 +1,29 @@
+describe('Test app', function() {
+    'use strict';
+
+    beforeEach(function() {
+        this.server = sinon.fakeServer.create();
+        this.body = window.document.body;
+
+        // create container
+        var container = window.document.createElement('div');
+        container.id = 'parentContainer';
+
+        // append to body
+        this.body.appendChild(container);
+    });
+
+    afterEach(function() {
+        this.server.restore();
+    });
+
+    it('Test gameMenu method', function() {
+        var gameMenu = new GameMenu(),
+            container = document.getElementById('parentContainer');
+
+        gameMenu.appendMenu(container);
+
+        expect(container.childNodes.length).toBeGreaterThan(0);
+        expect(container.childNodes[0].id).toBe('gameMenu');
+    });
+});
