ef4cd9d8f105e77aa1ec141714c5c57b23dd971b
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-09-04 18:37:39 +0200

    Improve app

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-define('app', ['consts'], function(consts) {
+define('app', ['consts', 'buttons'], function(consts, buttons) {
     'use strict';
 
     class _GameMenu {
@@ -14,7 +14,25 @@ define('app', ['consts'], function(consts) {
         }
     }
 
+    class _StartButtons {
+        constructor() {
+            this.start = buttons.START;
+            this.load = buttons.LOAD;
+            this.exit = buttons.EXIT;
+        }
+        appendStartButton(parent) {
+            parent.appendChild(this.start);
+        }
+        appendLoadButton(parent) {
+            parent.appendChild(this.load);
+        }
+        appendExitButton(parent) {
+            parent.appendChild(this.exit);
+        }
+    }
+
     return {
-        GameMenu: _GameMenu
+        GameMenu: _GameMenu,
+        StartButtons: _StartButtons
     }
 });
