5d53714d05228358698cc7679903f3f38241739b
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-09-04 17:32:05 +0200

    Improve app

--- app/app.js
+++ app/app.js
@@ -1,14 +1,23 @@
-'use strict';
+define('app', [], function(app) {
+    'use strict';
 
-const doc = window.document,
-    docBody = doc.body;
+    const doc = window.document,
+        docBody = doc.body;
 
-class GameMenu {
-    constructor() {
-        this.gameMenu = doc.createElement('div');
-        this.gameMenu.id = 'gameMenu';
+    class _GameMenu {
+        constructor() {
+            this.gameMenu = doc.createElement('div');
+            this.gameMenu.id = 'gameMenu';
+        }
+        getParent() {
+            return window.document.getElementById('parentContainer');
+        }
+        appendMenu(parent) {
+            parent.appendChild(this.gameMenu);
+        }
     }
-    appendMenu(parent) {
-        parent.appendChild(this.gameMenu);
+
+    return {
+        GameMenu: _GameMenu
     }
-}
+}());
