35e121f9f6c53c4e222d4ae1b9967d92be6745ed
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-09-04 18:02:52 +0200

    Created consts.js

---
+++ app/consts.js
@@ -0,0 +1,12 @@
+define('consts', [], function() {
+    'use strict';
+
+    const
+        _DOC = window.document,
+        _DOCBODY = _DOC.body;
+
+    return {
+        DOC: _DOC,
+        DOCBODY: _DOCBODY
+    }
+});
