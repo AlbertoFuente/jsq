cc07f0b785e3b41f9394bbeb8171c03c1039f92c
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-09-04 18:03:11 +0200

    Implemented consts dependency

--- app/config.js
+++ app/config.js
@@ -3,15 +3,17 @@
 
     require.config({
         baseUrl: 'app',
-        shim: {
+        paths: {
+            'consts': 'consts',
             'app': 'app'
         },
         callback: function() {
             'use strict';
 
-            require(['app'], function(app) {
+            require(['app', 'consts'], function(app, consts) {
                 var menu = new app.GameMenu(),
                     parent = menu.getParent();
+
                 menu.appendMenu(parent);
             });
         }
