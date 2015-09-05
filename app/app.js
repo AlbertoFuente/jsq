2d57682f4b644af59f12d97987702d7fa6a9e05d
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-09-05 16:51:45 +0200

    Implemented init function

--- app/app.js
+++ app/app.js
@@ -7,7 +7,10 @@ define('app', ['consts', 'buttons'], function(consts, buttons) {
             this.gameMenu.id = 'gameMenu';
         }
         getParent() {
-            return window.document.getElementById('parentContainer');
+            return consts.DOC.getElementById('parentContainer');
+        }
+        getGameMenu() {
+            return this.gameMenu;
         }
         appendMenu(parent) {
             parent.appendChild(this.gameMenu);
@@ -31,7 +34,21 @@ define('app', ['consts', 'buttons'], function(consts, buttons) {
         }
     }
 
+    let _init = () => {
+        let menu = new _GameMenu(),
+            parent = menu.getParent(),
+            startButtons = new _StartButtons(),
+            parentGameMenu = menu.getGameMenu();
+
+        menu.appendMenu(parent);
+
+        startButtons.appendStartButton(parentGameMenu);
+        startButtons.appendLoadButton(parentGameMenu);
+        startButtons.appendExitButton(parentGameMenu);
+    }
+
     return {
+        init: _init,
         GameMenu: _GameMenu,
         StartButtons: _StartButtons
     }
