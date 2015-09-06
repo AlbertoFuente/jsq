6c4535268a5b61c9fd9e055ff3da19add6e8a2e6
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-09-06 08:13:55 +0200

    Improve app

---
+++ app/app.js
@@ -0,0 +1,20 @@
+define('app', ['consts', 'buttons', 'components'], function(consts, buttons, components) {
+    'use strict';
+
+    let _init = () => {
+        let menu = new components.GameMenu(),
+            parent = menu.getParent(),
+            startButtons = new components.StartButtons(),
+            parentGameMenu = menu.getGameMenu();
+
+        menu.appendMenu(parent);
+
+        startButtons.appendStartButton(parentGameMenu);
+        startButtons.appendLoadButton(parentGameMenu);
+        startButtons.appendExitButton(parentGameMenu);
+    };
+
+    return {
+        init: _init
+    }
+});
