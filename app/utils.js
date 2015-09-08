3179e37f22a1f896718c3815f906fd17ad3f75f5
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-09-08 20:21:33 +0200

    Added range method

--- app/utils.js
+++ app/utils.js
@@ -2,17 +2,31 @@ define('utils', ['consts'], function(consts) {
     'use strict';
 
     let _emptyContainer = (container) => {
-        let element = consts.DOC.getElementById(container),
-            elementChilds = element.childNodes;
+            let element = consts.DOC.getElementById(container),
+                elementChilds = element.childNodes;
 
-        Array.from(elementChilds).forEach((key) => {
-            if (key.tagName === 'DIV') {
-                element.removeChild(key);
+            Array.from(elementChilds).forEach((key) => {
+                if (key.tagName === 'DIV') {
+                    element.removeChild(key);
+                }
+            });
+        },
+        _range = (start, end, step) => {
+            let range = [];
+
+            if (typeof start === 'number' && typeof end === 'number') {
+                range[0] = start;
+                step = step || 1;
+
+                while (start + step <= end) {
+                    range[range.length] = start += step;
+                }
             }
-        });
-    }
+            return range;
+        }
 
     return {
-        emptyContainer: _emptyContainer
+        emptyContainer: _emptyContainer,
+        range: _range
     }
 });
