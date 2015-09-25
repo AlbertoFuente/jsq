d7a6ad039895ae4742c859e403ce9e099702cea8
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-09-25 21:14:38 +0200

    config changes

--- app/config.js
+++ app/config.js
@@ -4,6 +4,9 @@
     require.config({
         baseUrl: 'app',
         paths: {
+            // vendor
+            '$': '../node_modules/jquery/dist/jquery.min',
+            // app
             'utils': 'utils',
             'consts': 'consts',
             'buttons': 'buttons',
@@ -11,12 +14,31 @@
             'events': 'events',
             'app': 'app'
         },
+        deps: [
+            '$'
+        ],
         callback: function() {
             'use strict';
 
-            require(['utils', 'app', 'consts', 'buttons', 'events', 'components'], function(utils, app, consts, buttons, events, components) {
+            require([
+                'utils',
+                'app',
+                'consts',
+                'buttons',
+                'events',
+                'components'
+            ], function(utils, app, consts, buttons, events, components) {
                 app.init();
             });
         }
-    })
+    });
+
+    requirejs.onError = function(err) {
+        console.log(err.requireType);
+        if (err.requireType === 'timeout') {
+            console.log('modules: ' + err.requireModules);
+        }
+
+        throw err;
+    };
 }());
