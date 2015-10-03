1514c80682ab08422a4f9214dc6923f893c40b0a
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-10-03 21:17:52 +0200

    Improved gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,14 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     babel = require("gulp-babel"),
     sass = require('gulp-sass'),
+    eslint = require('gulp-eslint'),
     minifyCSS = require('gulp-minify-css'),
     rename = require('gulp-rename'),
     Server = require('karma').Server,
-    testFiles = ['js/app.min.js', 'spec/test.js'],
-    appFiles = ['app/consts.js', 'app/buttons.js', 'app/components.js', 'app/events.js', 'app/utils.js', 'app/app.js'],
+    // Files
+    testFiles = ['js/app.min.js', 'spec/*.js'],
+    testJsFiles = ['spec/*.js'],
+    appFiles = ['app/*.js'],
     sassFiles = ['styles/styles.scss'];
 
 gulp.task('default', function() {
@@ -16,6 +19,8 @@ gulp.task('default', function() {
 
     gulp.start('js');
     gulp.start('sass');
+    gulp.start('eslint');
+    gulp.start('eslintTestFiles');
     gulp.start('karma');
 });
 
@@ -50,12 +55,25 @@ gulp.task('karma', function() {
     }).start();
 });
 
+gulp.task('eslint', function() {
+    return gulp.src(appFiles)
+        .pipe(eslint('.eslintrc'))
+        .pipe(eslint.format());
+});
+
+gulp.task('eslintTestFiles', function() {
+    return gulp.src(testJsFiles)
+        .pipe(eslint('.eslintrc'))
+        .pipe(eslint.format());
+});
+
 gulp.task('watch', function() {
     'use strict';
 
     gulp.watch(appFiles,
         function() {
             gulp.start('js');
+            gulp.start('eslint');
         });
     gulp.watch(sassFiles,
         function() {
@@ -63,6 +81,7 @@ gulp.task('watch', function() {
         });
     gulp.watch(testFiles,
         function() {
+            gulp.start('eslintTestFiles');
             gulp.start('karma');
         });
 });
