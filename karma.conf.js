62ba549c1d9c6b2644ea36a1a112e0c57b037599
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-11-15 18:54:11 +0100

    Working on travis ci config

--- karma.conf.js
+++ karma.conf.js
@@ -69,6 +69,19 @@ module.exports = function(config) {
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false
+        singleRun: false,
+
+        // This is the new content for your travis-ci configuration test
+        //  Custom launcher for Travis-CI
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
     });
+
+    if (process.env.TRAVIS) {
+        config.browsers = ['Chrome_travis_ci'];
+    }
 };
